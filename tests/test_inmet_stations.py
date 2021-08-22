df52150bc8f902662fc0893cac4d4a9ad1a63b0a
Felippe Galdino <ocfgaldino@gmail.com>
2021-08-22 01:02:58 -0300

    Changes in structure to test dir. First test file added

---
+++ tests/test_inmet_stations.py
@@ -0,0 +1,14 @@
+import unittest
+
+from inmetpy.stations.inmet_stations import InmetStation
+
+class TestInmetStation(unittest.TestCase):
+    
+    def test_list_stations(self):
+        self
+    
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
